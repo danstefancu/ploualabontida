67041263b2b60880081786c96634ad8597da1173
Dan Stefancu <dan@stefancu.ro>
2016-07-08 19:37:59 +0300

    New: dev class for staging.

--- site/web/app/themes/ploualabontida/index.php
+++ site/web/app/themes/ploualabontida/index.php
@@ -15,7 +15,7 @@
 	</style>
 </head>
 <body>
-<div class="message">
+<div class="message dev">
 
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<p><?php the_title(); ?></p>
