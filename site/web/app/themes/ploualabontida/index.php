ff2bd477af2d2ec5517b6ac29d902883ead586c6
Dan Stefancu <dan@stefancu.ro>
2016-07-11 01:14:19 +0300

    New: footer template.

--- site/web/app/themes/ploualabontida/index.php
+++ site/web/app/themes/ploualabontida/index.php
@@ -12,16 +12,8 @@ get_header();
 	<?php endwhile; endif; ?>
 
 </div>
-<div>
-	<script type="text/javascript">
-		//<![CDATA[
-		var sc_project=10498948;
-		var sc_invisible=0;
-		var sc_security="0ecf9343";
-		var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.");
-		document.write("<sc"+"ript type='text/javascript' src='" +scJsHost+"statcounter.com/counter/counter_xhtml.js'></"+"script>");
-		//]]>
-	</script>
-	<noscript><div class="statcounter"><a title="web counter" href="http://statcounter.com/" class="statcounter"><img class="statcounter" src="http://c.statcounter.com/10498948/0/0ecf9343/0/" alt="web counter" /></a></div></noscript>
-</div>
-</body>
+<?php
+
+get_footer();
+
+?>
