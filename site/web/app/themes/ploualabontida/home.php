0b9060cf9d8f9488f6b648d3733171710102f3db
Dan Stefancu <dan@stefancu.ro>
2016-07-11 02:12:46 +0300

    New: single page template and a fix for date display.

--- site/web/app/themes/ploualabontida/home.php
+++ site/web/app/themes/ploualabontida/home.php
@@ -8,7 +8,7 @@ get_header();
 	<h1>Un istoric</h1>
 
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<p><a href="<?php the_permalink(); ?>"><?php the_date(); ?></a></p>
+		<p><a href="<?php the_permalink(); ?>"><?php the_time('j F Y'); ?></a></p>
 	<?php endwhile; endif; ?>
 
 </div>
---
+++ site/web/app/themes/ploualabontida/single.php
@@ -0,0 +1,23 @@
+<?php
+
+get_header();
+
+?>
+<div class="message">
+
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<p><?php the_title(); ?></p>
+		<?php $post = get_post(); ?>
+		<h1><?php echo $post->post_content; ?></h1>
+	<?php endwhile; endif; ?>
+
+</div>
+<div>
+	<a href="<?php echo get_post_type_archive_link( 'post' ); ?>">Istorie</a> | <a href="<?php echo home_url(); ?>">Vremea la Bontida</a>
+</div>
+
+<?php
+
+get_footer();
+
+?>
