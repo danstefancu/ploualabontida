23f4fa268184f99dd80776a0a6948f947543214d
Dan Stefancu <dan@stefancu.ro>
2016-07-11 01:43:40 +0300

    New: template for archives.

---
+++ site/web/app/themes/ploualabontida/home.php
@@ -0,0 +1,20 @@
+<?php
+
+get_header();
+
+?>
+<div class="message">
+
+	<h1>Un istoric</h1>
+
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<p><a href="<?php the_permalink(); ?>"><?php the_date(); ?></a></p>
+	<?php endwhile; endif; ?>
+
+</div>
+
+<?php
+
+get_footer();
+
+?>
