039a1b8124bf6dc1dc1c6211711e91ce2708042c
Dan Stefancu <dan@stefancu.ro>
2016-07-25 18:26:59 +0300

    Update: page counter.

--- site/web/app/themes/ploualabontida/footer.php
+++ site/web/app/themes/ploualabontida/footer.php
@@ -8,6 +8,6 @@
 		document.write("<sc"+"ript type='text/javascript' src='" +scJsHost+"statcounter.com/counter/counter_xhtml.js'></"+"script>");
 		//]]>
 	</script>
-	<noscript><div class="statcounter"><a title="web counter" href="http://statcounter.com/" class="statcounter"><img class="statcounter" src="http://c.statcounter.com/10498948/0/0ecf9343/0/" alt="web counter" /></a></div></noscript>
+	<noscript><div class="statcounter"><a title="counter" href="http://statcounter.com/p10498948/?guest=1" class="statcounter"><img class="statcounter" src="//c.statcounter.com/10498948/0/0ecf9343/0/" alt="counter" /></a></div></noscript>
 </div>
 </body>
