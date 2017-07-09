6acf7df03f80a25c718fa5c3d28ce24a96ff1309
Dan Stefancu <dan@stefancu.ro>
2017-07-09 14:20:55 +0300

    New: facebook share button.

--- site/web/app/themes/ploualabontida/footer.php
+++ site/web/app/themes/ploualabontida/footer.php
@@ -10,4 +10,5 @@
 	</script>
 	<noscript><div class="statcounter"><a title="counter" href="http://statcounter.com/p10498948/?guest=1" class="statcounter"><img class="statcounter" src="//c.statcounter.com/10498948/0/0ecf9343/0/" alt="counter" /></a></div></noscript>
 </div>
+<div style="display: block;" class="fb-share-button" data-href="http://ploualabontida.com" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fploualabontida.com%2F&amp;src=sdkpreparse">Share</a></div>
 </body>
